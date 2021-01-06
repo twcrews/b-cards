2c2d825f0ac35664b039a01738974ddcf9cb0c08
Thomas Crews <twcrews96@gmail.com>
2021-01-05 22:46:31 -0600

    Added button to enable a custom action.

--- src/components/EmptyState.js
+++ src/components/EmptyState.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
-import Typography from '@material-ui/core/Typography';
+import * as M from '@material-ui/core';
 
 export default function EmptyState(props) {
     return (
         <div className="EmptyState">
             <SearchIcon style={{ fontSize: 200 }}/>
-            <Typography variant="h6">{props.children}</Typography>
+            <M.Typography
+                variant="h6"
+            >
+                {props.children}
+            </M.Typography>
+            <M.Button 
+                variant="contained" 
+                onClick={props.onClick}
+                color="primary"
+                startIcon={props.buttonIcon}
+            >
+                {props.button}
+            </M.Button>
         </div>
     );
 }
